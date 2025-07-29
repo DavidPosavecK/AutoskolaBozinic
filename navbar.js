498af8399dd0a677c7259d530965c1d1de0d9652
David Posavec Kovač <d.posaveckovac@gmail.com>
2025-07-29 14:28:46 +0200

    Hamburger menu radi i popravljen

--- navbar.js
+++ navbar.js
@@ -5,3 +5,10 @@ function toggleMenu() {
   navLinks.classList.toggle("show");
   hamburger.classList.toggle("active");
 }
+
+const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('navLinks');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
